d516489b3532e54cf23900244fa1f6128406e265
Kristina <your.email@example.com>
2025-04-05 13:24:40 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const counterElement = document.getElementById('counter'); 
+const incrementButton = document.getElementById('increment'); 
+const decrementButton = document.getElementById('decrement'); 
+ 
+let count = 0; 
+ 
+incrementButton.addEventListener('click', () => { 
+    count++; 
+    counterElement.textContent = count; 
+}); 
+ 
+decrementButton.addEventListener('click', () => { 
+    count--; 
+    counterElement.textContent = count; 
+});
